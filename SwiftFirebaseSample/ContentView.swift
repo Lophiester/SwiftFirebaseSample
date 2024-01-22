7d87881f77a4668d55c8d670f20de886b922c122
lophiester <gilcharles2022@hotmail.com>
2024-01-22 17:49:56 +0900

    FireBase init

---
+++ SwiftFirebaseSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftFirebaseSample
+//
+//  Created by Charles Yamamoto on 2024/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftFirebaseSample/SwiftFirebaseSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFirebaseSampleApp.swift
+//  SwiftFirebaseSample
+//
+//  Created by Charles Yamamoto on 2024/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFirebaseSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
